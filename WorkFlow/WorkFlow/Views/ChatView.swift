d3d1a87e66188eaac0ba919c07b86fe4bb700a37
Heriberto Hernandez <geolal121@gmail.com>
2024-10-14 14:23:52 -0700

    Revert "Hyhernandez"

--- WorkFlow/WorkFlow/Views/ChatView.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import SwiftUI
-
-struct ChatView: View {
-    var body: some View {
-        Text("Chat View")
-            .navigationBarTitle("Chat", displayMode: .inline)
-    }
-}

598721fb448e2279553c6dbf1110b4f54e4dab05
Steve Coyotl <steve.coyotl@csuci.edu>
2024-11-12 18:19:43 -0800

    Fixed fatal error bug

--- WorkFlow/WorkFlow/Views/ContractorViews/CoProfileView.swift
+++ WorkFlow/WorkFlow/Views/ContractorViews/CoProfileView.swift
@@ -13,7 +13,7 @@ struct IdentifiableErrorCO: Identifiable {
 struct ContractorProfileView: View {
     @Environment(\.presentationMode) var presentationMode
     @EnvironmentObject var authController: AuthController
-    @EnvironmentObject var contractorController1: ContractorController
+    @EnvironmentObject var contractorController: ContractorController
     @State private var profileImage: Image? = Image("profilePlaceholder")
     @State private var name: String = ""
     @State private var location: String = ""
@@ -128,7 +128,7 @@ struct ContractorProfileView: View {
                 self.profilePictureURL = data["profilePictureURL"] as? String
                 loadProfileImage()
                 
-                contractorController1.fetchFlyersForContractor(contractorId: userId)
+                contractorController.fetchFlyersForContractor(contractorId: userId)
                 self.isLoading = false
             } else {
                 self.errorMessage = IdentifiableErrorCO(message: "User data not found.")
@@ -268,7 +268,7 @@ struct ContractorProfileView: View {
                 .foregroundColor(.white)
                 .padding(.bottom, 5)
 
-            ForEach(contractorController1.contractorFlyers, id: \.id) { flyer in
+            ForEach(contractorController.contractorFlyers, id: \.id) { flyer in
                 NavigationLink(destination: FlyerDetailView(contractor: flyer)) {
                     HStack {
                         if let imageURL = flyer.imageURL, let url = URL(string: imageURL) {
@@ -347,6 +347,8 @@ struct ContractorProfileView: View {
                         .environmentObject(AuthController())
                         .environmentObject(JobController())
                         .environmentObject(FlyerController())
+                        .environmentObject(BidController())
+                        .environmentObject(ContractorController())
                 )
                 window.makeKeyAndVisible()
             }
